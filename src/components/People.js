880c7e24a8e5431521dc148340166886690bc980
Shivayan Bora <shivayan.bora@britive.com>
2022-05-25 12:38:13 +0530

    Added React Query

--- src/components/People.js
+++ src/components/People.js
@@ -1,7 +1,30 @@
+import { useQuery } from "react-query";
+import Person from "./Person";
+
+const fetchPeople = async () => {
+  const res = await fetch("https://swapi.dev/api/people/");
+  return res.json();
+};
+
 const People = () => {
+  // The useQuery hook is used to fetch data from the API.
+  // The first argument is the name of the query, the second is the function that will fetch the data.
+  // The third argument is an object that contains options for the query.
+  // The query will be automatically re-run when the component is re-rendered.
+  const { data, status } = useQuery("people", fetchPeople);
+
   return (
     <div>
       <h2>People</h2>
+      {status === "error" && <div>Error fetching data!</div>}
+      {status === "loading" && <div>Loading data...</div>}
+      {status === "success" && (
+        <div>
+          {data?.results?.map((person, index) => (
+            <Person person={person} key={index} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,7 +1,30 @@
+import { useQuery } from "react-query";
+import Planet from "./Planet";
+
+const fetchPlanets = async () => {
+  const res = await fetch("https://swapi.dev/api/planets/");
+  return res.json();
+};
+
 const Planets = () => {
+  // The useQuery hook is used to fetch data from the API.
+  // The first argument is the name of the query, the second is the function that will fetch the data.
+  // The third argument is an object that contains options for the query.
+  // The query will be automatically re-run when the component is re-rendered.
+  const { data, status } = useQuery("planets", fetchPlanets);
+
   return (
     <div>
       <h2>Planets</h2>
+      {status === "error" && <div>Error fetching data!</div>}
+      {status === "loading" && <div>Loading data...</div>}
+      {status === "success" && (
+        <div>
+          {data?.results?.map((planet, index) => (
+            <Planet planet={planet} key={index} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
