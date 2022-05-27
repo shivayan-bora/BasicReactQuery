9f487ac32e7627dcb4adc00069f107996a20d920
Shivayan Bora <shivayan.bora@britive.com>
2022-05-27 10:11:34 +0530

    Added Pagination in the data

--- src/components/People.js
+++ src/components/People.js
@@ -1,20 +1,27 @@
+import { useState } from "react";
 import { useQuery } from "react-query";
 import Person from "./Person";
 
-const fetchPeople = async () => {
-  const res = await fetch("https://swapi.dev/api/people/");
+const fetchPeople = async (page = 1) => {
+  const res = await fetch(`https://swapi.dev/api/people/?page=${page}`);
   return res.json();
 };
 
 const People = () => {
+  const [page, setPage] = useState(1);
+
   // The useQuery hook is used to fetch data from the API.
   // The first argument is the name of the query, the second is the function that will fetch the data.
   // The third argument is an object that contains options for the query.
   // The query will be automatically re-run when the component is re-rendered.
-  const { data, status } = useQuery("people", fetchPeople, {
-    onSuccess: () => console.log("People fetched successfully"),
-    onError: () => console.log("Error fetching people"),
-  });
+  const { data, status, isPreviousData } = useQuery(
+    ["people", page],
+    () => fetchPeople(page),
+    {
+      onSuccess: () => console.log("People fetched successfully"),
+      onError: () => console.log("Error fetching people"),
+    }
+  );
 
   return (
     <div>
@@ -22,11 +29,30 @@ const People = () => {
       {status === "error" && <div>Error fetching data!</div>}
       {status === "loading" && <div>Loading data...</div>}
       {status === "success" && (
-        <div>
-          {data?.results?.map((person, index) => (
-            <Person person={person} key={index} />
-          ))}
-        </div>
+        <>
+          <button
+            onClick={() => setPage((old) => Math.max(old - 1, 1))}
+            disabled={page === 1}
+          >
+            Previous Page
+          </button>
+          <span>{page}</span>
+          <button
+            onClick={() => {
+              if (!isPreviousData && data.next) {
+                setPage((old) => old + 1);
+              }
+            }}
+            disabled={isPreviousData || !data?.next}
+          >
+            Next Page
+          </button>
+          <div>
+            {data?.results?.map((person, index) => (
+              <Person person={person} key={index} />
+            ))}
+          </div>
+        </>
       )}
     </div>
   );
